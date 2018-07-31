ed6c6ceebdc87b2174188be61a5bc6f24798f9b3
drewatienza <atienza.drew@gmail.com>
2018-07-30 18:21:28 -0700

    created a scroll component to scroll the friends list while keeping the searchbar visible

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import SearchBox from './SearchBox'
+import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
@@ -15,15 +16,16 @@ class App extends Component {
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(users => this.setState({ robots: users }));
+      .then(users => {this.setState({ robots: users})});
   }
 
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value });
   }
+
   render () {
-    const filteredRobots = this.state.robots.filter(robot =>{
-      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    const filteredRobots = this.state.robots.filter(robots =>{
+      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     });
     if (this.state.robots.length === 0) {
       return <h1>Loading</h1>
@@ -32,7 +34,9 @@ class App extends Component {
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
-          <CardList robots={filteredRobots}/>
+          <Scroll>
+            <CardList robots={filteredRobots}/>
+          </Scroll>
         </div>
       );
     }
